feb54d90aa1331228bda1cedc516c4be2f1f25ed
MaddoSaru <riquelme.madmonkey@gmail.com>
2023-09-14 11:37:00 +0800

    feat(mysql_test/): add new features

---
+++ mysql_test/utils/insert_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO {table_name} {field_names} VALUES {replacement_values}
--- mysql_test/utils/querys.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    *,
-    CAST(CURRENT_TIMESTAMP AS DATETIME) AS uploaded_at
-FROM
-    {table_name}
-ORDER BY
-    id
